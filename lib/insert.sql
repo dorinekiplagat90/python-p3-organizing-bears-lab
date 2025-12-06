9fc39465065358bb61459c1c329df07a88420152
dorinekiplagat90 <dorinekiplagat90@gmail.com>
2025-12-06 21:46:25 +0300

    added a file

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+("Mr. Chocolate", 20, "M", "Brown", "Calm", 1),
+("Rowdy", 15, "M", "Black", "Aggressive", 1),
+("Tabitha", 8, "F", "White", "Sweet", 1),
+("Sergeant Brown", 18, "M", "Brown", "Strict", 1),
+("Melissa", 7, "F", "Black", "Gentle", 1),
+("Grinch", 12, "M", "Green", "Grumpy", 1),
+("Wendy", 9, "F", "Brown", "Happy", 1),
+(NULL, 3, "M", "Grey", "Neutral", 0);
